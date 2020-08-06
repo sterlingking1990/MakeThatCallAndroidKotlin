0237b33f4ee57b4b8e0b430c7224d6a3644b5e20
sterlingking1990 <izundukingsleyemeka@gmail.com>
2020-08-06 20:55:45 +0100

    add condition for when no number is entered

--- app/src/main/java/com/example/callintent/MainActivity.kt
+++ app/src/main/java/com/example/callintent/MainActivity.kt
@@ -5,6 +5,7 @@ import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -17,8 +18,14 @@ class MainActivity : AppCompatActivity() {
 
         btnCall.setOnClickListener {
             try {
-                val intent = Intent(Intent.ACTION_DIAL, Uri.parse("tel:$phone_number"))
-                startActivity(intent)
+                if(phone_number.isEmpty()){
+                    Toast.makeText(this,"Phone number should be entered",Toast.LENGTH_SHORT).show()
+
+                }
+                else {
+                    val intent = Intent(Intent.ACTION_DIAL, Uri.parse("tel:$phone_number"))
+                    startActivity(intent)
+                }
             } catch (e: Exception) {
                 Log.i("error", e.printStackTrace().toString())
             }
