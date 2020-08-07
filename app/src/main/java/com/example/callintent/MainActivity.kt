e4690da1276e8330082f7e3a4a8552de9f12e6d1
sterlingking1990 <izundukingsleyemeka@gmail.com>
2020-08-07 08:20:42 +0100

    complete call app

--- app/src/main/java/com/example/callintent/MainActivity.kt
+++ app/src/main/java/com/example/callintent/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.callintent
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.text.Editable
 import android.util.Log
 import android.widget.EditText
 import android.widget.Toast
@@ -14,21 +15,32 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //access the phone number entered from the  edit text view
         var phone_number=findViewById<EditText>(R.id.etPhoneNumber).text
 
+        //set on click listener and declare and intent to make call
         btnCall.setOnClickListener {
-            try {
-                if(phone_number.isEmpty()){
-                    Toast.makeText(this,"Phone number should be entered",Toast.LENGTH_SHORT).show()
-
+                //if phone number is empty send a toast
+                if(isNumberEmpty(phone_number)){
+                    Toast.makeText(this,"Phone number should be entered and in proper format",Toast.LENGTH_SHORT).show()
                 }
+                //if the user entered phone number
                 else {
+                    //trigger an implicit intent to make call using the action dial
                     val intent = Intent(Intent.ACTION_DIAL, Uri.parse("tel:$phone_number"))
-                    startActivity(intent)
+
+                    //check if there exist a call app that can receive the intent to avoid app crashing - ref: https://developer.android.com/guide/components/intents-common
+                    if (intent.resolveActivity(packageManager) != null) {
+                        startActivity(intent)
+                    }
+                    else{
+                        Toast.makeText(this,"Please install a call app to initiate this process",Toast.LENGTH_SHORT).show()
+                    }
                 }
-            } catch (e: Exception) {
-                Log.i("error", e.printStackTrace().toString())
-            }
         }
+    }
+
+    private fun isNumberEmpty(num:Editable):Boolean{
+     return num.isEmpty()
     }
 }
